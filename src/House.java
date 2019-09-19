66da218ec841b985c3f047669776f165cb845763
shevcari000 <shevcari000@CAM-LAB-27.bgsd.k12.wa.us>
2019-09-19 07:44:15 -0700

    Started

--- src/House.java
+++ src/House.java
@@ -3,7 +3,43 @@ public class House {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        HouseJackBuilt();
+        Space();
+        Malt();
+        LayInHouse();
+        Space();
+        Rat();
+        AteMalt();
+        LayInHouse();
+        Space();
+        Cat();
+        KilledRat();
+        AteMalt();
+        LayInHouse();
+        Space();
+        Dog();
+        WorriedCat();
+        KilldRat();
+        AteMalt();
+        LayInHouse();
+        Space();
+        Cow();
+        TossedDog();
+        WorriedCat();
+        KilledRat();
+        AteMalt();
+        LayInHouse();
+        Space();
+        Maiden();
+        MilkedCow();
+        TossedDog();
+        WorriedCat();
+        KilledRat();
+        AteMalt();
+        LayInHouse();
+        
 	}
+	
+	
 
 }
