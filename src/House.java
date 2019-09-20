f714ace15eb43d0506c1115e9e68af5d5d6558c7
shevcari000 <shevcari000@CAM-LAB-27.bgsd.k12.wa.us>
2019-09-20 07:25:05 -0700

    Finished

--- src/House.java
+++ src/House.java
@@ -39,72 +39,85 @@ public class House {
         
 	}
 	
-	public class static void HouseJackBuilt () {
+	public  static void HouseJackBuilt () {
 		System.out.println ("This is the house that Jack built.");
+	}
 	
-	public class static void Malt () {
+	public  static void Malt () {
 	    System.out.println ("This is the malt");
+	}
 
   
-    public class static void Space () {
+    public static void Space () {
     	System.out.println ("");
+    }
     	
    
     
-    public class static void LayInHouse () {
+    public static void LayInHouse () {
     	System.out.println ("That lay in the house that jack built.");
+    }
     	
     
     
-    public class static void Rat () {
+    public static void Rat () {
     	System.out.println ("This is the rat,");
+    }
  
     
     
-    public class static void AteMalt () {
+    public static void AteMalt () {
     	System.out.println ("That ate the malt");
+    }
     	
     
     
-    public class static void Cat () {
+    public static void Cat () {
     	System.out.println ("This is the cat,");
+    }
     	
     
     
-    public class static void KilledRat () {
+    public static void KilledRat () {
     	System.out.println ("That killed the rat,");
+    }
     
     
     
-    public class static void Dog () {
+    public static void Dog () {
     	System.out.println ("This is the dog,");
+    }
     	
    
    
-    public class static void WorriedCat () {
+    public static void WorriedCat () {
     	System.out.println ("That worried the cat,");
+    }
     	
    
     
-    public class static void Cow () {
+    public static void Cow () {
     	System.out.println ("This the cow with crumpled horn,");
+    }
     	
    
   
-    public class static void TossedDog () {
+    public static void TossedDog () {
     	System.out.println ("That tossed the dog");
+    }
     	
    
     
-    public class static void Maiden () {
+    public static void Maiden () {
     	System.out.println ("This is the maiden all forlorn");
+    }
     	
    
     
-    public class static void MilkedCow () {
+    public static void MilkedCow () {
     	System.out.println ("That milked the cow with the crumpled horn,");
     	
     }
 }
-    }}}}}}}}}}}} 
+    
     
