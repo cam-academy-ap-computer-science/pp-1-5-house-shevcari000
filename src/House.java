a47ff1b8b92c5ae4125cfe123a63aa3afee96f07
Admin <Admin@DESKTOP-DM8MVBD.hsd1.wa.comcast.net>
2019-09-19 18:22:24 -0700

    Updated
    Signed-off-by: Admin <Admin@DESKTOP-DM8MVBD.hsd1.wa.comcast.net>

--- src/House.java
+++ src/House.java
@@ -2,7 +2,6 @@
 public class House {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
         HouseJackBuilt();
         Space();
         Malt();
@@ -19,7 +18,7 @@ public class House {
         Space();
         Dog();
         WorriedCat();
-        KilldRat();
+        KilledRat();
         AteMalt();
         LayInHouse();
         Space();
@@ -42,15 +41,70 @@ public class House {
 	
 	public class static void HouseJackBuilt () {
 		System.out.println ("This is the house that Jack built.");
-	}
 	
 	public class static void Malt () {
 	    System.out.println ("This is the malt");
 
-    }
+  
     public class static void Space () {
     	System.out.println ("");
     	
-    }
+   
     
+    public class static void LayInHouse () {
+    	System.out.println ("That lay in the house that jack built.");
+    	
+    
+    
+    public class static void Rat () {
+    	System.out.println ("This is the rat,");
+ 
+    
+    
+    public class static void AteMalt () {
+    	System.out.println ("That ate the malt");
+    	
+    
+    
+    public class static void Cat () {
+    	System.out.println ("This is the cat,");
+    	
+    
+    
+    public class static void KilledRat () {
+    	System.out.println ("That killed the rat,");
+    
+    
+    
+    public class static void Dog () {
+    	System.out.println ("This is the dog,");
+    	
+   
+   
+    public class static void WorriedCat () {
+    	System.out.println ("That worried the cat,");
+    	
+   
+    
+    public class static void Cow () {
+    	System.out.println ("This the cow with crumpled horn,");
+    	
+   
+  
+    public class static void TossedDog () {
+    	System.out.println ("That tossed the dog");
+    	
+   
+    
+    public class static void Maiden () {
+    	System.out.println ("This is the maiden all forlorn");
+    	
+   
+    
+    public class static void MilkedCow () {
+    	System.out.println ("That milked the cow with the crumpled horn,");
+    	
+    }
+}
+    }}}}}}}}}}}} 
     
